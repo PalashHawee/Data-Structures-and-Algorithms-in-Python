2607bdd172ee0044110282077e37736f5b412f43
Palash Hawee <67676859+PalashHawee@users.noreply.github.com>
2021-07-16 18:26:52 +0530

    Quick Sort

---
+++ Sorting Techniques/QuickSort.py
@@ -0,0 +1,23 @@
+#Implementing Quick Sort Algorithm using backward process
+def partition(arr,low,high):
+	i=low-1
+	pivot=arr[high]
+	for j in range(low,high):
+		if arr[j]<=pivot:
+			i=i+1
+			arr[i],arr[j]=arr[j],arr[i]
+		arr[i+1],arr[high]=arr[high],arr[i+1]
+		
+def quickSort(arr,low,high):
+	if len(arr)==1:
+		return arr
+	if low<high:
+		pi=partition(arr,low,high)
+		quickSort(arr,low,pi-1)
+		quickSort(arr,pi+1,high)
+
+arr=[12,8,7,6,93,5]
+n=len(arr)
+quickSort(arr,0,n-1)
+for i in range(n):
+	print("%d"% arr[i])						
