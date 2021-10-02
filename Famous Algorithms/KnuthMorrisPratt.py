9a2924ac6ed656c79a9b9e54dd8c74855be90bd7
Palash Hawee <67676859+PalashHawee@users.noreply.github.com>
2021-10-02 20:01:59 +0530

    Knuth Morris Pratt (KMP) Algo

---
+++ Famous Algorithms/KnuthMorrisPratt.py
@@ -0,0 +1,36 @@
+#Knuth Morris Pratt Algorithm for String Matching (KMP)
+#Time O(M+N) where m is the length of the substring & n is the length of the String
+#Space O(M) length of the input substring
+
+def knuthMorrisPrattAlgorithm(string, substring):
+    
+    pattern=buildPattern(substring)
+	return doesMatch(string,substring,pattern)
+def buildPattern(substring):
+	pattern=[-1 for i in substring]
+	j=0
+	i=1
+	while i <len(substring):
+		if substring[i]==substring[j]:
+			pattern[i]=j
+			i+=1
+			j+=1
+		elif j > 0:
+			j=pattern[j-1]+1
+		else:
+			i+=1
+	return pattern
+def doesMatch(string,substring,pattern):
+	i=0
+	j=0
+	while i+len(substring) -j <= len(string):
+		if string[i]==substring[j]:
+			if j==len(substring)-1:
+				return True
+			i+=1
+			j+=1
+		elif j > 0:
+			j=pattern[j-1]+1
+		else:
+			i+=1
+	return False		
