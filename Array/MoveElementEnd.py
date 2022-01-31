19b3a641fa68b21b0e18d39d6f4e49a67184ec93
Palash Hawee <67676859+PalashHawee@users.noreply.github.com>
2022-01-31 18:40:30 +0530

    Moving Element to End

---
+++ Array/MoveElementEnd.py
@@ -0,0 +1,16 @@
+#Time O(N) and Space O(1)
+#Moving Element to end in place
+
+def moveElementToEnd(array, toMove):
+    
+    i=0
+	j=len(array)-1
+	while i < j:
+		while i < j and array[j]==toMove:
+			j-=1
+		if array[i]==toMove:
+			array[i], array[j]=array[j], array[i]
+		i+=1
+	return array	
+		
+		
