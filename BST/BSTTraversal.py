e014654ecc6f348e43332aa79e36c8a47ff05276
Palash Hawee <67676859+PalashHawee@users.noreply.github.com>
2022-05-26 19:23:05 +0530

    BST Traversal

---
+++ BST/BSTTraversal.py
@@ -0,0 +1,30 @@
+#BST Traversal
+
+#time O (n) | space o(n)
+
+def inOrderTraverse(tree, array):
+    
+    if tree is not None:
+        inOrderTraverse(tree.left,array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right,array)
+    return array    
+
+
+def preOrderTraverse(tree, array):
+    # Write your code here.
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left,array)
+        preOrderTraverse(tree.right,array)
+    return array
+    
+
+
+def postOrderTraverse(tree, array):
+    # Write your code here.
+    if tree is not None:
+        postOrderTraverse(tree.left,array)
+        postOrderTraverse(tree.right,array)
+        array.append(tree.value)
+    return array    
